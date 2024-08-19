d2ff7e38ea3dc77137be31ba99cf4a0ae222bfad
danialchoudary <danialchoudary255@gmail.com>
2024-08-19 16:40:45 +0500

    Update Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
           ))}
         </div>
         <div className="md:hidden">
-          <button onClick={handleToggle} className="text-black focus:outline-none">
+          <button onClick={handleToggle} className="text-white focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
